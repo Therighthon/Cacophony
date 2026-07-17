cf1802950aaf4ca2dcab4267532a79f5c30e49bd
Therighthon <52086285+Therighthon@users.noreply.github.com>
2026-07-17 13:03:10 -0700

    A modest number of crickets

---
+++ src/main/java/com/therighthon/cacophony/common/ranges/SaltMarshRanges.java
@@ -0,0 +1,99 @@
+package com.therighthon.cacophony.common.ranges;
+
+import com.therighthon.cacophony.common.DayTime;
+import com.therighthon.cacophony.common.Sounds;
+import java.util.List;
+import java.util.Locale;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.level.biome.Biome;
+
+import net.dries007.tfc.util.climate.KoppenClimateClassification;
+
+public enum SaltMarshRanges implements RegistryRange
+{
+    WESTERN_MEADOWLARK_WINTER(Sounds.WESTERN_MEADOWLARK.get(), 10, List.of(DayTime.DAWN, DayTime.DAY, DayTime.DUSK), List.of(Biome.Precipitation.NONE), List.of(KoppenClimateClassification.CSB, KoppenClimateClassification.CSC, KoppenClimateClassification.CWB, KoppenClimateClassification.CWC, KoppenClimateClassification.CFC, KoppenClimateClassification.CFB), 0.75f, 0.25f, 60, 200),
+    WESTERN_MEADOWLARK_SUMMER(Sounds.WESTERN_MEADOWLARK.get(), 10, List.of(DayTime.DAWN, DayTime.DAY, DayTime.DUSK), List.of(Biome.Precipitation.NONE), List.of(KoppenClimateClassification.DSA, KoppenClimateClassification.DSB, KoppenClimateClassification.DSC, KoppenClimateClassification.DWA, KoppenClimateClassification.DWB, KoppenClimateClassification.DWC, KoppenClimateClassification.DFA, KoppenClimateClassification.DFB, KoppenClimateClassification.DFC), 0.25f, 0.75f, 60, 200),
+    TWO_COLORED_OVAL_FROG(Sounds.TWO_COLORED_OVAL_FROG.get(), 1, List.of(DayTime.DUSK, DayTime.DARK), List.of(Biome.Precipitation.NONE, Biome.Precipitation.RAIN), List.of(KoppenClimateClassification.CFA, KoppenClimateClassification.CFB, KoppenClimateClassification.CFC), 0f, 1f, 60, 200);
+
+    private final String serializedName;
+    final SoundEvent sound;
+    final List<DayTime> times;
+    final List<Biome.Precipitation> weathers;
+    final List<KoppenClimateClassification> climates;
+    final float startYearFraction, endYearFraction;
+    final int minElevation, maxElevation, chance;
+
+    SaltMarshRanges(SoundEvent sound, int chance, List<DayTime> times, List<Biome.Precipitation> weathers, List<KoppenClimateClassification> climates, float startYearFraction, float endYearFraction, int minElevation, int maxElevation)
+    {
+        this.serializedName = name().toLowerCase(Locale.ROOT);
+        this.sound = sound;
+        this.chance = chance;
+        this.times = times;
+        this.weathers = weathers;
+        this.climates = climates;
+        this.startYearFraction = startYearFraction;
+        this.endYearFraction = endYearFraction;
+        this.minElevation = minElevation;
+        this.maxElevation = maxElevation;
+    }
+
+    @Override
+    public List<KoppenClimateClassification> validClimates()
+    {
+        return climates;
+    }
+
+    @Override
+    public List<DayTime> validDayTimes()
+    {
+        return times;
+    }
+
+    @Override
+    public List<Biome.Precipitation> validWeathers()
+    {
+        return weathers;
+    }
+
+    @Override
+    public SoundEvent sound()
+    {
+        return sound;
+    }
+
+    @Override
+    public float startYearFraction()
+    {
+        return startYearFraction;
+    }
+
+    @Override
+    public float endYearFraction()
+    {
+        return endYearFraction;
+    }
+
+    @Override
+    public int getMinElevation()
+    {
+        return minElevation;
+    }
+
+    @Override
+    public int getMaxElevation()
+    {
+        return maxElevation;
+    }
+
+    @Override
+    public int getChance()
+    {
+        return chance;
+    }
+
+    @Override
+    public String getSerializedName()
+    {
+        return serializedName;
+    }
+}

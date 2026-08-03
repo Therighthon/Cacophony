e037215698a8a4492eb2fd4cb5cc585ee426bb48
Therighthon <52086285+Therighthon@users.noreply.github.com>
2026-08-02 19:45:16 -0700

    Migrate to TFC's tags

--- src/main/java/com/therighthon/cacophony/common/CacophonyTags.java
+++ src/main/java/com/therighthon/cacophony/common/CacophonyTags.java
@@ -5,16 +5,18 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 
+import net.dries007.tfc.util.Helpers;
+
 import static net.dries007.tfc.util.Helpers.*;
 
 public class CacophonyTags
 {
     public static class Blocks
     {
-        // TODO: On TFC Update
+        // TFC Tags
         public static final TagKey<Block> FRESH_EMERGENT_PLANTS = blockTag("fresh_emergent_plants");
         public static final TagKey<Block> FRESH_FLOATING_PLANTS = blockTag("fresh_floating_plants");
-        public static final TagKey<Block> TALL_GRASS = blockTag("tall_grass");
+        public static final TagKey<Block> TALL_GRASS = blockTag("tall_grasses");
         public static final TagKey<Block> DRY_PLANTS = blockTag("dry_plants");
         public static final TagKey<Block> CACTI = blockTag("cacti");
 
@@ -25,19 +27,19 @@ public class CacophonyTags
     {
         public static final TagKey<Biome> IS_SURFACE_LAKE = biomeTag("is_surface_lake");
         public static final TagKey<Biome> IS_SALT_MARSH = biomeTag("is_salt_marsh");
-        public static final TagKey<Biome> IS_CLIFFY_SHORE = biomeTag("is_cliffy_shore");
+        public static final TagKey<Biome> IS_CLIFFY_SHORE = biomeTag("is_coastal_cliffs");
         public static final TagKey<Biome> IS_OCEANIC_MOUNTAINS = biomeTag("is_oceanic_mountains");
         public static final TagKey<Biome> IS_ISOLATED_ISLAND = biomeTag("is_isolated_island");
     }
 
     private static TagKey<Block> blockTag(String name)
     {
-        return TagKey.create(Registries.BLOCK, CacophonyHelpers.identifier(name));
+        return TagKey.create(Registries.BLOCK, Helpers.identifier(name));
     }
 
     private static TagKey<Biome> biomeTag(String name)
     {
-        return TagKey.create(Registries.BIOME, CacophonyHelpers.identifier(name));
+        return TagKey.create(Registries.BIOME, Helpers.identifier(name));
     }
 
     private static TagKey<Block> mcTag(String name)
